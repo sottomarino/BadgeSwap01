54cfc5f775aff44dc0aa33aa3f05a674b3ed450c
simone ambrosi <70901023+sottomarino@users.noreply.github.com>
2022-01-29 08:21:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Web3Modal from "web3modal"
 import Image from 'next/image'
 
 import {
-  nftaddress, nftmarketaddress
+  nftaddress, nftmarketaddress, rpc_url
 } from '../config'
 
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
@@ -24,7 +24,7 @@ export default function Home() {
     loadNFTs()
   }, [])
   async function loadNFTs() {    
-    const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint)
+    const provider = new ethers.providers.JsonRpcProvider(rpc_url)
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
