9a6097c4b6f27c95a00577ac966c8dbe74552ba1
simone ambrosi <70901023+sottomarino@users.noreply.github.com>
2022-01-29 09:17:14 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,11 @@
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
-
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-
-  let txHash, txReceipt
   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
   const nftMarket = await NFTMarket.deploy();
   await nftMarket.deployed();
-
-  txHash = nftMarket.deployTransaction.hash;
-  txReceipt = await ethers.provider.waitForTransaction(txHash);
-  let nftMarketAddress = txReceipt.contractAddress
-
-  console.log("nftMarket deployed to:", nftMarketAddress);
+  console.log("nftMarket deployed to:", nftMarket.address);
 
   const NFT = await hre.ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy(nftMarketAddress);
+  const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
-
-
-  txHash = nft.deployTransaction.hash;
-  // console.log(`NFT hash: ${txHash}\nWaiting for transaction to be mined...`);
-  txReceipt = await ethers.provider.waitForTransaction(txHash);
-  let nftAddress = txReceipt.contractAddress
-
-  console.log("nft deployed to:", nftAddress);
+  console.log("nft deployed to:", nft.address);
 }
